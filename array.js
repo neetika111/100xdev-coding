b539781292d119b8956542a88a1c729a58c39be2
neetika111 <neetika244@example.com>
2025-03-05 16:22:07 +0530

    add initial commit

---
+++ array.js
@@ -0,0 +1,23 @@
+let person1 = "Neetika";
+let person2 = "Tanya";
+let person3 = "Pooja";
+
+const person = ["Neetika","Tanya","Pooja"];
+
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+const ages = [24,25,26,92,89];
+const no = ages.length;
+const max=0;
+for(let i=0;i<no;i++)   // age.length is not defined
+{
+    
+    if(ages[i]>ages[i+1])
+    {
+        max=ages[i];
+    }
+    
+}    
+console.log(max);
---
+++ object.js
@@ -0,0 +1,23 @@
+// const personarray =["neetika","neha","neeraj","neha","neetika","neeraj"];
+// const genderarray =["male","female","male","female","male","female"];
+// const personarraylength = personarray.length;
+
+// for(let i=0;i<personarraylength;i++)
+// {
+//     if(genderarray[i]=="male")
+//     {
+//         console.log(personarray[i]);
+//     }
+// }
+
+const user1 ={
+    firstName: "Neetika",
+    lastName: "Sharma",
+    age: 24,
+    isMarried: false
+}
+
+console.log(user1.firstName);
+console.log(user1.lastName);    
+console.log(user1.age);
+console.log(user1.isMarried);
